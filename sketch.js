82013cf4f8103e5fd22592afb7f198323675e44a
Ophilia Fradarick <76613993+OphiliaFradarick@users.noreply.github.com>
2022-08-30 21:00:21 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 
 var gamestate = "level1";
+var letterCount = 0;
+var a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z;
 
 function preload() {
 	backgroundImg = loadImage("wood background.jpg");
@@ -46,9 +48,83 @@ function setup() {
 	cat = createSprite(100, 600)
 	cat.addImage(catImg)
 	cat.scale = 0.3
+	cat.debug = true;
+	cat.setCollider("circle", 0, 10, 120)
 
 	ground2 = createSprite(0, 680, 800, 10);
 	ground2.visible = false;
+
+	a = createSprite(850, Math.round(random(70, 590)))
+	a.addImage(letterA)
+	a.scale = 0.2
+	a.debug = true;
+	aGroup = new Group();
+	//A.velocityX = -(random(4, 6))
+
+	c = createSprite(850, Math.round(random(70, 590)))
+	c.addImage(letterC)
+	c.scale = 0.2;
+	c.debug = true;
+	cGroup = new Group();
+	//C.velocityX = -(random(4, 6))
+
+	d = createSprite(850, Math.round(random(70, 590)))
+	d.addImage(letterD)
+	d.scale = 0.2
+	d.debug = true;
+	dGroup = new Group();
+	//D.velocityX = -(random(4, 6))
+
+	e = createSprite(850, Math.round(random(70, 590)))
+	e.addImage(letterE)
+	e.scale = 0.2
+	e.debug = true;
+	eGroup = new Group();
+	//E.velocityX = -(random(4, 6))
+
+	g = createSprite(850, Math.round(random(70, 590)))
+	g.addImage(letterG)
+	g.scale = 0.2
+	g.debug = true;
+	gGroup = new Group();
+	//G.velocityX = -(random(4, 6))
+
+	n = createSprite(850, Math.round(random(70, 590)))
+	n.addImage(letterN)
+	n.scale = 0.2
+	n.debug = true;
+	nGroup = new Group();
+	//N.velocityX = -(random(4, 6))
+
+	o = createSprite(850, Math.round(random(70, 590)))
+	o.addImage(letterO)
+	o.scale = 0.2
+	o.debug = true;
+	oGroup = new Group();
+	//O.velocityX = -(random(4, 6))
+
+
+	r = createSprite(850, Math.round(random(70, 590)))
+	r.addImage(letterR)
+	r.scale = 0.2
+	r.debug = true;
+	rGroup = new Group();
+	//R.velocityX = -(random(4, 6))
+
+	s = createSprite(850, Math.round(random(70, 590)))
+	s.addImage(letterS)
+	s.scale = 0.2
+	s.debug = true;
+	sGroup = new Group();
+	//S.velocityX = -(random(4, 6))
+
+
+	w = createSprite(850, Math.round(random(70, 590)))
+	w.addImage(letterW)
+	w.scale = 0.2
+	w.debug = true;
+	wGroup = new Group();
+	//W.velocityX = -(random(4, 6))
 
 }
 
@@ -73,126 +149,94 @@ function draw() {
 		cat.y = cat.y + 6;
 	}
 
-	if (gamestate === "level1") {
-		textSize(40);
-		fill("white");
-		stroke("black");
-		strokeWeight(3);
-		text("CATCH THE WORD - O R A N G E", 100, 200);
-		spawnTheWordORANGE()
-	}
+	textSize(40);
+	fill("white");
+	stroke("black");
+	strokeWeight(3);
+	text("CATCH THE WORD - O R A N G E", 100, 200);
+	text("LETTER COUNT - " + letterCount, 190, 270);
 
-	cat.overlap(ground2);
+	spawnTheWordORANGE();
+
+	// if (gamestate === "level1") {
+	// 	textSize(40);
+	// 	fill("white");
+	// 	stroke("black");
+	// 	strokeWeight(3);
+	// 	text("CATCH THE WORD - O R A N G E", 100, 200);
+	// 	text("LETTER COUNT - " +letterCount, 190, 270);
+	// 	spawnTheWordORANGE()
+	// }
+
 
 }
 
 function spawnTheWordORANGE() {
-	if (frameCount % 50 == 0) {
+	if (frameCount % 10 === 0) {
 
-		A = createSprite(810, Math.round(random(70, 590)))
-		A.addImage(letterA)
-		A.scale = 0.2
-		A.velocityX = -(random(4, 6))
+		a.velocityX = -(random(4, 6))
+		aGroup.add(a);
 
+		c.velocityX = -(random(1, 6))
+		cGroup.add(c);
 
+		d.velocityX = -(random(4, 6))
+		dGroup.add(d);
 
-		C = createSprite(810, Math.round(random(70, 590)))
-		C.addImage(letterC)
-		C.scale = 0.2;
-		C.velocityX = -(random(4, 6))
+		e.velocityX = -(random(2, 6))
+		eGroup.add(e);
 
-		D = createSprite(810, Math.round(random(70, 590)))
-		D.addImage(letterD)
-		D.scale = 0.2
-		D.velocityX = -(random(4, 6))
+		g.velocityX = -(random(4, 6))
+		gGroup.add(g);
 
-		E = createSprite(810, Math.round(random(70, 590)))
-		E.addImage(letterE)
-		E.scale = 0.2
-		E.velocityX = -(random(4, 6))
+		n.velocityX = -(random(2, 6))
+		nGroup.add(n);
 
+		o.velocityX = -(random(1, 6))
+		oGroup.add(o);
 
+		r.velocityX = -(random(2, 6))
+		rGroup.add(r);
 
-		G = createSprite(810, Math.round(random(70, 590)))
-		G.addImage(letterG)
-		G.scale = 0.2
-		G.velocityX = -(random(4, 6))
+		s.velocityX = -(random(4, 6))
+		sGroup.add(s);
 
+		w.velocityX = -(random(1, 6))
+		wGroup.add(w);
 
-
-		N = createSprite(810, Math.round(random(70, 590)))
-		N.addImage(letterN)
-		N.scale = 0.2
-		N.velocityX = -(random(4, 6))
-
-		O = createSprite(810, Math.round(random(70, 590)))
-		O.addImage(letterO)
-		O.scale = 0.2
-		O.velocityX = -(random(4, 6))
-
-
-		R = createSprite(810, Math.round(random(70, 590)))
-		R.addImage(letterR)
-		R.scale = 0.2
-		R.velocityX = -(random(4, 6))
-
-		S = createSprite(810, Math.round(random(70, 590)))
-		S.addImage(letterS)
-		S.scale = 0.2
-		S.velocityX = -(random(4, 6))
-
-
-		W = createSprite(810, Math.round(random(70, 590)))
-		W.addImage(letterW)
-		W.scale = 0.2
-		W.velocityX = -(random(4, 6))
-
-		if (cat.overlap(O)) {
-			textSize(47);
-			fill("green");
-			stroke("black");
-			strokeWeight(3);
-			text("O", 80, 270);
+		if (oGroup.isTouching(cat)) {
+            oGroup[0].destroy();
+			letterCount += 1;
 		}
 
-		else if (cat.overlap(R)) {
-			textSize(47);
-			fill("green");
-			stroke("black");
-			strokeWeight(3);
-			text("R", 86, 270);
+		else if (rGroup.isTouching(cat)) {
+           
+			letterCount += 1;
+			rGroup[0].destroy();
 		}
 
-		else if (cat.overlap(A)) {
-			textSize(47);
-			fill("green");
-			stroke("black");
-			strokeWeight(3);
-			text("A", 90, 270);
+		else if (aGroup.isTouching(cat)) {
+
+			letterCount += 1;
+			aGroup[0].destroy();
 		}
 
-		else if (cat.overlap(N)) {
-			textSize(47);
-			fill("green");
-			stroke("black");
-			strokeWeight(3);
-			text("N", 95, 270);
+		else if (nGroup.isTouching(cat)) {
+
+			letterCount += 1;
+			nGroup[0].destroy();
 		}
 
-		else if (cat.overlap(G)) {
-			textSize(47);
-			fill("green");
-			stroke("black");
-			strokeWeight(3);
-			text("G", 100, 270);
+		else if (gGroup.isTouching(cat)) {
+
+			letterCount += 1;
+			gGroup[0].destroy();
 		}
 
-		else if (cat.overlap(E)) {
-			textSize(47);
-			fill("green");
-			stroke("black");
-			strokeWeight(3);
-			text("E", 105, 270);
+		else if (eGroup.isTouching(cat)) {
+
+			letterCount += 1;
+			eGroup[0].destroy();
 		}
 
 
